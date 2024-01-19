bdaeccb025b0835319153cc2db7010f50ae77ee0
ammann64 <austinmmann64@gmail.com>
2024-01-18 20:49:06 -0600

    Change db name in connection.js, and remove extraneous model references in seeds.js

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -3,25 +3,21 @@ const { User } = require('../models')
 const cleanDB = require('./cleanDB')
 
 db.once('open', async () => {
-	await cleanDB('Category', 'categories')
-	await cleanDB('Product', 'products')
-	await cleanDB('User', 'users')
+	await cleanDB('User', 'users');
 
 	await User.create({
-		firstName: 'Pamela',
-		lastName: 'Washington',
+		username: 'pamwham16',
 		email: 'pamela@testmail.com',
 		password: 'password12345'
 	})
 
 	await User.create({
-		firstName: 'Elijah',
-		lastName: 'Holt',
+		username: 'holtyourhorses26',
 		email: 'eholt@testmail.com',
 		password: 'password12345'
 	})
 
-	console.log('users seeded')
+	console.log('users seeded');
 
 	process.exit()
 })
