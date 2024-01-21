af825663e7290413e85839437b8bed9d2b023a6d
clayg <claytondguerrero@gmail.com>
2024-01-20 19:22:27 -0600

    found tailwind issue

--- client/src/components/Nav/index.jsx
+++ client/src/components/Nav/index.jsx
@@ -1,7 +1,33 @@
-import { Link } from 'react-router-dom'
 import Auth from '../../utils/auth'
+import React from 'react'
+import {
+	Navbar,
+	NavbarBrand,
+	NavbarContent,
+	NavbarItem,
+	NavbarMenu,
+	NavbarMenuToggle,
+	NavbarMenuItem,
+	Link,
+	Button
+} from '@nextui-org/react'
 
 function Nav() {
+
+	const [isMenuOpen, setIsMenuOpen] = React.useState(false)
+	  const menuItems = [
+			'Profile',
+			'Dashboard',
+			'Activity',
+			'Analytics',
+			'System',
+			'Deployments',
+			'My Settings',
+			'Team Settings',
+			'Help & Feedback',
+			'Log Out'
+		]
+
 	function showNavigation() {
 		if (Auth.loggedIn()) {
 			return (
@@ -34,20 +60,76 @@ function Nav() {
 	}
 
 	return (
-		<header className='flex-row px-1'>
-			<h1>
-				<Link to='/'>
-					<span
-						role='img'
-						aria-label='shopping bag'>
-						🛍️
-					</span>
-					-Shop-Shop
-				</Link>
-			</h1>
+		<Navbar onMenuOpenChange={setIsMenuOpen}>
+			<NavbarContent>
+				<NavbarMenuToggle
+					aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
+					className='sm:hidden'
+				/>
+				<NavbarBrand>
+					<p className='font-bold text-inherit'>ACME</p>
+				</NavbarBrand>
+			</NavbarContent>
 
-			<nav>{showNavigation()}</nav>
-		</header>
+			<NavbarContent
+				className='hidden sm:flex gap-4'
+				justify='center'>
+				<NavbarItem>
+					<Link
+						color='foreground'
+						href='#'>
+						Features
+					</Link>
+				</NavbarItem>
+				<NavbarItem isActive>
+					<Link
+						href='#'
+						aria-current='page'>
+						Customers
+					</Link>
+				</NavbarItem>
+				<NavbarItem>
+					<Link
+						color='foreground'
+						href='#'>
+						Integrations
+					</Link>
+				</NavbarItem>
+			</NavbarContent>
+			<NavbarContent justify='end'>
+				<NavbarItem className='hidden lg:flex'>
+					<Link href='#'>Login</Link>
+				</NavbarItem>
+				<NavbarItem>
+					<Button
+						as={Link}
+						color='primary'
+						href='#'
+						variant='flat'>
+						Sign Up
+					</Button>
+				</NavbarItem>
+			</NavbarContent>
+			<NavbarMenu>
+				{menuItems.map((item, index) => (
+					<NavbarMenuItem key={`${item}-${index}`}>
+						<Link
+							color={
+								index === 2
+									? 'primary'
+									: index === menuItems.length - 1
+										? 'danger'
+										: 'foreground'
+							}
+							className='w-full'
+							href='#'
+							size='lg'>
+							{item}
+						</Link>
+					</NavbarMenuItem>
+				))}
+			</NavbarMenu>
+		</Navbar>
 	)
 }
 
