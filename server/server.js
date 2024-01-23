787d11ccface73b19c31bc910746fbd5cb6d5397
tgaytan <tgaytan@users.noreply.github.com>
2024-01-23 02:35:07 +0000

    Format code with Prettier

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-const express = require("express")
+const express = require('express')
 const { ApolloServer } = require('@apollo/server')
 const { expressMiddleware } = require('@apollo/server/express4')
 const path = require('path')
--- server/utils/s3.js
+++ server/utils/s3.js
@@ -1,45 +1,45 @@
 const {
-    S3Client,
-    PutObjectCommand,
-    DeleteObjectCommand
+	S3Client,
+	PutObjectCommand,
+	DeleteObjectCommand
 } = require('@aws-sdk/client-s3')
 
 const dotenv = require('dotenv')
 
 dotenv.config()
 
-const accessKey=process.env.ACCESS_KEY
-const secretAccessKey=process.env.SECRET_ACCESS_KEY
-const bucketName=process.env.BUCKET_NAME
-const bucketRegion=process.env.BUCKET_REGION
+const accessKey = process.env.ACCESS_KEY
+const secretAccessKey = process.env.SECRET_ACCESS_KEY
+const bucketName = process.env.BUCKET_NAME
+const bucketRegion = process.env.BUCKET_REGION
 
 const s3Client = new S3Client({
-    credentials: {
-        accessKeyId: accessKey,
-        secretAccessKey: secretAccessKey
-    },
-    region: bucketRegion
+	credentials: {
+		accessKeyId: accessKey,
+		secretAccessKey: secretAccessKey
+	},
+	region: bucketRegion
 })
 
 module.exports = {
-    addObject: async function(fileName) {
-        await s3Client.send(
-            new PutObjectCommand({
-                Bucket: bucketName,
-                Key: fileName,
-                Body: "Hello JavaScript SDK!"
-            })
-        )
-    },
-    deleteObject: async function(fileName) {
-        await s3Client.send(
-            new DeleteObjectCommand({
-                Bucket: bucketName,
-                Key: fileName
-            })
-        )
-    },
-    generateObjectUrl: async function(fileName) {
-        return `https://${bucketName}.s3.${bucketRegion}.amazonaws.com/${fileName}`
-    }
+	addObject: async function (fileName) {
+		await s3Client.send(
+			new PutObjectCommand({
+				Bucket: bucketName,
+				Key: fileName,
+				Body: 'Hello JavaScript SDK!'
+			})
+		)
+	},
+	deleteObject: async function (fileName) {
+		await s3Client.send(
+			new DeleteObjectCommand({
+				Bucket: bucketName,
+				Key: fileName
+			})
+		)
+	},
+	generateObjectUrl: async function (fileName) {
+		return `https://${bucketName}.s3.${bucketRegion}.amazonaws.com/${fileName}`
+	}
 }
