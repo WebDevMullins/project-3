d5a3179dc1c48f73b94548583028aa26bae2a088
WebDevMullins <webdevmullins@gmail.com>
2024-01-22 19:18:10 -0600

    Update HeroSection image source and alt text

--- client/src/components/HeroSection/index.jsx
+++ client/src/components/HeroSection/index.jsx
@@ -7,8 +7,8 @@ const HeroSection = () => {
 				<div className='grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12'>
 					<div className='hidden lg:mt-0 lg:col-span-5 lg:flex'>
 						<img
-							src='./images/HeroFaded.png'
-							alt='mockup'
+							src='./images/Hero.png'
+							alt='AiImage'
 						/>
 					</div>
 					<div className='ml-auto place-self-center lg:col-span-7'>
