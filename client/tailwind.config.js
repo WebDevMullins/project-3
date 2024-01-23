b5464fd5d47ef7ddb15bd0c674f717f2de5ab5f5
WebDevMullins <WebDevMullins@users.noreply.github.com>
2024-01-23 00:39:01 +0000

    Format code with Prettier

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,11 @@ import { nextui } from '@nextui-org/react'
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'],
+	content: [
+		'./index.html',
+		'./src/**/*.{js,ts,jsx,tsx}',
+		'./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
+	],
 	theme: {
 		extend: {}
 	},
