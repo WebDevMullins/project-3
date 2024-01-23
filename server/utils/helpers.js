248de6d8e926ea23e87cb8b52a38c4dfd493332f
ammann64 <austinmmann64@gmail.com>
2024-01-22 21:14:29 -0600

    Set up openAI image generation in resolver

---
+++ server/utils/helpers.js
@@ -0,0 +1,13 @@
+const openAI = require('openai')
+const openai = new openAI({ apiKey: process.env.OPENAI_API_KEY })
+
+async function generateImage({ prompt, color, style, count }) {
+    const prompt = `${prompt}. Primarily colored ${color}. In a ${style} style.`
+    const image = await openai.images.generate({ model: "dall-e-3", prompt, n: parseInt(count) });
+
+    console.log(image.data);
+
+    return image.data
+}
+
+module.exports = generateImage;
