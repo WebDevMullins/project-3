6690ae3bf3cc0f0747c74446a9314c7495bdaccb
ammann64 <ammann64@users.noreply.github.com>
2024-01-26 03:13:19 +0000

    Format code with Prettier

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,22 +1,30 @@
 import { gql } from '@apollo/client'
 
 export const QUERY_SINGLE_USER = gql`
-    query singleUser($_id: ID!) {
-        user(_id: $_id) {
-            _id
-            firstName
-            lastName,
-            icons {_id, url, prompt}
-        }
-    }
+	query singleUser($_id: ID!) {
+		user(_id: $_id) {
+			_id
+			firstName
+			lastName
+			icons {
+				_id
+				url
+				prompt
+			}
+		}
+	}
 `
 export const QUERY_ME = gql`
-    query currentUser {
-        me {
-            _id
-            firstName
-            lastName,
-            icons {_id, url, prompt}
-        }
-    } 
+	query currentUser {
+		me {
+			_id
+			firstName
+			lastName
+			icons {
+				_id
+				url
+				prompt
+			}
+		}
+	}
 `
