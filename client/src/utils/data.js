77e10c017860e2dad5e4f2ba4c96bc03a15afae8
WebDevMullins <webdevmullins@gmail.com>
2024-01-28 18:54:10 -0600

    Refactor ColorPicker component and Generate page

--- client/src/utils/data.js
+++ client/src/utils/data.js
@@ -1,27 +1,21 @@
-const count = [
-	{
-		value: '1',
-		label: '1 Icon'
-	},
-	{
-		value: '2',
-		label: '2 Icons'
-	},
-	{
-		value: '3',
-		label: '3 Icons'
-	},
-	{
-		value: '4',
-		label: '4 Icons'
-	},
-	{
-		value: '5',
-		label: '5 Icons'
-	}
+const presetColors = [
+	{ hex: '#000000', name: 'Black' },
+	{ hex: '#C0C0C0', name: 'Silver' },
+	{ hex: '#808080', name: 'Gray' },
+	{ hex: '#FFFFFF', name: 'White' },
+	{ hex: '#800000', name: 'Maroon' },
+	{ hex: '#FF0000', name: 'Red' },
+	{ hex: '#800080', name: 'Purple' },
+	{ hex: '#FF00FF', name: 'Fuchsia' },
+	{ hex: '#008000', name: 'Green' },
+	{ hex: '#00FF00', name: 'Lime' },
+	{ hex: '#808000', name: 'Olive' },
+	{ hex: '#FFFF00', name: 'Yellow' },
+	{ hex: '#000080', name: 'Navy' },
+	{ hex: '#0000FF', name: 'Blue' },
+	{ hex: '#008080', name: 'Teal' },
+	{ hex: '#00FFFF', name: 'Aqua' }
 ]
-
-const presetColors = ['#cd9323', '#1a53d8', '#9a2151', '#0d6416', '#8d2808']
 
 const styles = [
 	'metallic',
@@ -50,4 +44,4 @@ const styles = [
 	'woodcut'
 ]
 
-export { count, presetColors, styles }
+export { presetColors, styles }
