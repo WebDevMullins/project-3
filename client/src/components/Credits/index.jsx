d219975047ce625fd53727538817995affccb252
WebDevMullins <webdevmullins@gmail.com>
2024-01-29 18:11:53 -0600

    Fix typo in Credits component

--- client/src/components/Credits/index.jsx
+++ client/src/components/Credits/index.jsx
@@ -7,7 +7,7 @@ const Credits = ({ credits='500' }) => {
 				size={18}
 				className='text-primary'
 			/>
-			<p className='text-lg text-whtie font-bold'>{credits}</p>
+			<p className='text-lg text-white font-bold'>{credits}</p>
 		</div>
 	)
 }
